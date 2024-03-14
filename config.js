7d1ff86a47d575f359a0bb3c8d624fe491e88b04
Ale29x <163438606+Ale29x@users.noreply.github.com>
2024-03-14 15:26:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.owner = [
 ["5492266613038"], 
 ["593968263524"],
 ["33760536110"],
-["573147616444"]]
+["51931028003"]]
 
 global.mods = []
 global.prems = []
@@ -36,7 +36,7 @@ global.keepAliveRender = 0; //Solo valores: 1 o 0
 // ❰❰ methodCode ❱❱
 // [ES] > Agregue el número del Bot en "botNumberCode" si desea recibir código de 8 dígitos sin registrar el número en la consola.
 // [EN] > Add the Bot number in "botNumberCode" if you want to receive 8-digit code without registering the number in the console.
-global.botNumberCode = "" //example: "+59309090909"
+global.botNumberCode = "51931028003" //example: "+59309090909"
 global.confirmCode = "" // No tocar esto : Do not touch this line
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
